bff8e626292532d6e7a2f40c0d35b953607aaa13
bernhard Linn Hilmarsson <bennilinn@gmail.com>
2021-11-19 16:49:07 +0000

    wrong username

--- pages/css.tsx
+++ pages/css.tsx
@@ -10,7 +10,7 @@ import type { NextPage } from "next";
 const css: NextPage = () => {
   return (
     <Wrapper>
-      <h1> user : lb </h1>
+      <h1> user : a3 </h1>
       <h1> password : 6B97F534C330B5CC78D4CC23E01E48BE3377105B </h1>
     </Wrapper>
   );
