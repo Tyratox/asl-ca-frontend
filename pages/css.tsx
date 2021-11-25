24ceb392914cad9be707ed25d800ac62a0568cdb
Nico Hauser <me@tyratox.ch>
2021-11-25 14:26:45 +0100

    Update css.tsx

--- pages/css.tsx
+++ pages/css.tsx
@@ -1,12 +1,9 @@
 import { withApp } from "../components/AppWrapper";
 import React from "react";
-import Wrapper from "../components/Wrapper";
 import type { NextPage } from "next";
 
 const css: NextPage = () => {
-  return (
-    <h1 style={{ display: "the user a3 is special" }}></h1>
-  );
+  return <h1 style={{ display: "the user a3 is special" }}></h1>;
 };
 
 export default withApp(css);
